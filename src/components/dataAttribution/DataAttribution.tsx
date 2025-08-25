48cbf96209083c3e489e77b9b9f9e994c2a72225
Franco Chan <kinfaifrancochan@gmail.com>
2025-08-25 15:22:39 +0100

    change attribution link for new dataset

--- src/components/dataAttribution/DataAttribution.tsx
+++ src/components/dataAttribution/DataAttribution.tsx
@@ -4,10 +4,10 @@ const DataAttribution = () => {
   return (
     <div id="dataAttribution">
       <p>
-        The <a href="https://catalogue.ceda.ac.uk/uuid/af60720c1e404a9e9d2c145d2b2ead4e" target='_blank'>ESA AGB Dataset</a> is 
+        The <a href="https://catalogue.ceda.ac.uk/uuid/95913ffb6467447ca72c4e9d8cf30501" target='_blank'>ESA AGB Dataset</a> is 
         based on Sentinel satellite data and other sources, 
         and both the data and this set are free to use for all purposes. (
-        <a href="https://dap.ceda.ac.uk/neodc/esacci/biomass/data/agb/maps/v4.0/00README_catalogue_and_licence.txt?download=1" target='_blank'>Licence</a>)
+        <a href="https://dap.ceda.ac.uk/neodc/esacci/biomass/data/agb/maps/v6.0/00README_catalogue_and_licence.txt?download=1" target='_blank'>Licence</a>)
       </p>
       <p>
         <a href="https://oncra.org" target='_blank'>ONCRA </a> 

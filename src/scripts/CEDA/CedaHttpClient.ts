b85f8e795e7b7a109c37ad20c709f43788b4d926
Franco Chan <kinfaifrancochan@gmail.com>
2025-08-25 14:45:14 +0100

    Update to use the new ESA published data for 2022
    ESA data link: https://catalogue.ceda.ac.uk/uuid/95913ffb6467447ca72c4e9d8cf30501/
    
    The data format is still the same as before, so simply updating the url and available
    years are sufficient for this change.

--- src/scripts/CEDA/CedaHttpClient.ts
+++ src/scripts/CEDA/CedaHttpClient.ts
@@ -3,8 +3,8 @@ import { getSecondsElapsed } from "../TimeUtils";
 const retryTimeoutSeconds = 1;
 
 export const callCedaEndpoint = async (year: number, latIndexEnd: number, latIndexStart: number, lonIndexStart: number, lonIndexEnd: number, ): Promise<string | null> => {
-  const url ="https://dap.ceda.ac.uk/thredds/dodsC/neodc/esacci/biomass/data/agb/maps/v4.0/netcdf/" + 
-  `ESACCI-BIOMASS-L4-AGB-MERGED-100m-${year}-fv4.0.nc.ascii` + 
+  const url ="https://dap.ceda.ac.uk/thredds/dodsC/neodc/esacci/biomass/data/agb/maps/v6.0/netcdf/" + 
+  `ESACCI-BIOMASS-L4-AGB-MERGED-100m-${year}-fv6.0.nc.ascii` + 
   `?agb[0:1:0][${latIndexStart}:1:${latIndexEnd}][${lonIndexStart}:1:${lonIndexEnd}]`;
 
   const startTime = new Date();
